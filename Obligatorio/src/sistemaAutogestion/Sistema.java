255b5bb7826329992a115d9e5fa6752c2ba925e1
Sofia Fernandez <sofiafernandez03@gmail.com>
2023-04-13 19:35:41 -0300

    estructura y el bug de eliminarCliente

--- Obligatorio/src/sistemaAutogestion/Sistema.java
+++ Obligatorio/src/sistemaAutogestion/Sistema.java
@@ -4,17 +4,17 @@ import tads.*;
 import clases.*;
 
 public class Sistema implements IObligatorio {
-    
+
     private ListaSimple listaClientes;
     private ListaSimple listaProductos;
     private ListaSimple listaStockProducto;
-    
-    public void mostrarLista(){
+
+    public void mostrarLista() {
         //listaClientes.mostrar();
         listaProductos.mostrar();
         //System.out.println(listaClientes.obtenerElemento(new Cliente("87654321")));
     }
-    
+
     @Override
     public Retorno crearSistemaDeAutoservicio(int maxUnidadesDePedido) {
         Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
@@ -27,7 +27,7 @@ public class Sistema implements IObligatorio {
         }
         return r;
     }
-    
+
     @Override
     public Retorno agregarCliente(String nombre, String ci, int tel) {
         Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
@@ -40,107 +40,120 @@ public class Sistema implements IObligatorio {
         }
         return r;
     }
-    
+
     @Override
     public Retorno eliminarCliente(String ci) {
         Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
         if (listaClientes.existeElemento(new Cliente(ci))) {
-            //if() FALTA SI TIENE LISTA DE PEDIDOS, NO SUPE HACERLO PORQUE obtenerElemento devuelve un Object, no un Client y no me deja acceder a la listaPedidos
-            listaClientes.eliminarElemento(new Cliente(ci));
-             r.resultado = Retorno.Resultado.OK;
+            Nodo nuevo = listaClientes.obtenerElemento(new Cliente(ci));
+            Cliente nuevoCli = (Cliente) nuevo.getDato();
+             if(nuevoCli.getListaPedidos().esVacia()){
+                listaClientes.eliminarElemento(new Cliente(ci));
+                r.resultado = Retorno.Resultado.OK;
+            }
+            else{
+                r.resultado = Retorno.Resultado.ERROR_2;
+             }
         } else {
             r.resultado = Retorno.Resultado.ERROR_1;
         }
         return r;
+
     }
-    
+
     @Override
-    public Retorno agregarProducto(String nombre, String descripcion) {
+    public Retorno agregarProducto(String nombre, String descripcion
+    ) {
         Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
         if (listaProductos.existeElemento(new Producto(nombre))) {
             r.resultado = Retorno.Resultado.ERROR_1;
         } else {
-            listaProductos.agregarInicio(new Producto(nombre));
+            listaProductos.agregarInicio(new Producto(nombre, descripcion));
             r.resultado = Retorno.Resultado.OK;
         }
         return r;
     }
-    
+
     @Override
-    public Retorno eliminarProducto(String nombre) {
+    public Retorno eliminarProducto(String nombre
+    ) {
         Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
         if (listaProductos.existeElemento(new Producto(nombre))) {
             //if() FALTA SI ESTA EN LISTA DE PEDIDOS, 
             listaProductos.eliminarElemento(new Producto(nombre));
-             r.resultado = Retorno.Resultado.OK;
+            r.resultado = Retorno.Resultado.OK;
         } else {
             r.resultado = Retorno.Resultado.ERROR_1;
         }
         return r;
     }
-    
+
     @Override
-    public Retorno altaStockProducto(String nroProducto, int unidades) {
+    public Retorno altaStockProducto(String nroProducto, int unidades
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     //HASTA ACÁ PRIMERA ENTREGA
-    
-    
-    
     @Override
-    public Retorno aperturaDePedido(String ciCliente) {
+    public Retorno aperturaDePedido(String ciCliente
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
-    public Retorno agregarProductoAPedido(String ciCliente, int nroProducto, int unidades) {
+    public Retorno agregarProductoAPedido(String ciCliente, int nroProducto, int unidades
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
-    public Retorno deshacerPedido(String ciCliente, int cantAccionesDeshacer) {
+    public Retorno deshacerPedido(String ciCliente, int cantAccionesDeshacer
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
-    public Retorno cerrarPedido(String ciCliente) {
+    public Retorno cerrarPedido(String ciCliente
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
-    public Retorno procesarPedido(int cantPedidos) {
+    public Retorno procesarPedido(int cantPedidos
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Retorno listarClientes() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Retorno listarProductos() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Retorno listarPedidosAbiertos() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
-    public Retorno pedidosCerradosDeClientes(int ci) {
+    public Retorno pedidosCerradosDeClientes(int ci
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Retorno productosParaEntregar() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
     @Override
     public Retorno reporteDePedidosSolicitadosXCliente() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
